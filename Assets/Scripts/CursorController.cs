95386fee67855398397bffad3ec8f3d8aef48fdb
Andrew Chen <akchen@ualberta.ca>
2021-10-02 21:36:39 -0600

    cursor

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+    
+    public Texture2D pickupTexture;
+    public Texture2D defaultTexture;
+    public Texture2D holdTexture;
+
+    private bool isHover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(defaultTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isHover)
+        {
+            Cursor.SetCursor(holdTexture, Vector2.zero, CursorMode.Auto);
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            Cursor.SetCursor(defaultTexture, Vector2.zero, CursorMode.Auto);
+        }
+    }
+
+    public void SetCursorSelect()
+    {
+        Cursor.SetCursor(pickupTexture, Vector2.zero, CursorMode.Auto);
+        isHover = true;
+    }
+
+    public void SetCursorDeselect()
+    {
+        Cursor.SetCursor(defaultTexture, Vector2.zero, CursorMode.Auto);
+        isHover = false;
+
+    }
+}
