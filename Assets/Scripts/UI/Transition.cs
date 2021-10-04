bab543966521c3880356a684cfaaa545b6e017c0
akchen1 <43446512+akchen1@users.noreply.github.com>
2021-10-04 02:08:24 -0600

    Update Transition.cs

--- Assets/Scripts/UI/Transition.cs
+++ Assets/Scripts/UI/Transition.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Transition : MonoBehaviour
 {
     CanvasGroup canvas;
-    public float time = 2f;
+    public float time = 0.5f;
     float timer;
+    private bool fade = false;
     // Start is called before the first frame update
     void Start()
     {
         canvas = GetComponent<CanvasGroup>();
         timer = time;
+        Invoke("StartFade", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!fade)
+        {
+            return;
+        }
         if (timer < 0)
         {
             canvas.alpha = 0;
@@ -25,5 +31,10 @@ public class Transition : MonoBehaviour
         }
         canvas.alpha = timer / time;
         timer -= Time.deltaTime;
+    }
+
+    void StartFade()
+    {
+        fade = true;
     }
 }
